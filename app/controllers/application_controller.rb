afe1f0a07dad6e9542dfb64aaa27b80a0923b5ca
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-03-27 18:50:10 -0400

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,36 @@
 class ApplicationController < Sinatra::Base
-
+  # Add this line to set the Content-Type header for all responses
+  set :default_content_type, 'application/json'
   get '/' do
-    { message: "Hello world" }.to_json
+    { message: 'Hello world' }.to_json
   end
 
+  get '/games' do
+    #get all the games from the database
+    games = Game.all.order(:title).limit(10)
+
+    # return a JSON response with an array of all the game data
+    games.to_json
+  end
+
+  # use the :id syntax to create a dynamic route
+  get '/games/:id' do
+    # look up the game in the database using its ID
+    game = Game.find(params[:id])
+
+    # send a JSON-formatted response of the the game data
+    game.to_json(
+      only: %i[id title genre price],
+      include: {
+        reviews: {
+          only: %i[comment score],
+          include: {
+            user: {
+              only: [:name],
+            },
+          },
+        },
+      },
+    )
+  end
 end
